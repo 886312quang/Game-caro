6a34c1bb99e4016c05c776416f952da9aaf25b17
Quang Phạm Minh <886312quangpham@gmail.com>
2021-08-12 22:03:51 +0700

    init

---
+++ caro-reactjs-client-master/build/precache-manifest.caa776104a2fff1be7bc1e6edb82dd56.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0279b3c16056ff573a91551b240df0ae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "33028f10a510f0b974d0",
+    "url": "/static/css/main.dcd57483.chunk.css"
+  },
+  {
+    "revision": "24e8dc087da91551edf5",
+    "url": "/static/js/2.2f853e07.chunk.js"
+  },
+  {
+    "revision": "33028f10a510f0b974d0",
+    "url": "/static/js/main.5eef11d4.chunk.js"
+  },
+  {
+    "revision": "5237db175a82411b449d",
+    "url": "/static/js/runtime~main.d4a129de.js"
+  },
+  {
+    "revision": "1f04957a406504d8a4a8f8155d086ba0",
+    "url": "/static/media/facebook.1f04957a.png"
+  },
+  {
+    "revision": "f95f60bd6844a8b5fb644ed95075d7d6",
+    "url": "/static/media/google.f95f60bd.png"
+  },
+  {
+    "revision": "2487640df86fea813e4a7e9b4dc1548c",
+    "url": "/static/media/logo.2487640d.svg"
+  }
+]);
---
+++ caro-reactjs-client-master/src/config.js
@@ -0,0 +1,13 @@
+// These exports are for localhost
+var exports_1 = {
+  'client-domain': '//localhost:9999/',
+  'server-domain': '//localhost:3000/'
+};
+
+// These exports are for uploading to heroku
+var exports_2 = {
+  'client-domain': 'https://minhquangcaro9999.netlify.app/',
+  'server-domain': 'https://minhquangcaro.herokuapp.com/'
+};
+
+export default exports_1;
